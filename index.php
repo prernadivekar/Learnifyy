eda217feb3b690b47a68b66894d95e1786613ee5
prernadivekar <65289696+prernadivekar@users.noreply.github.com>
2023-10-18 14:05:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,577 @@
+<?php
+
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+	header('location:login.php');
+	# code...
+
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Aleo" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+	<title>Learnifyy</title>
+	<!----magnific popup css file for work section -->
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+	<!----owlcarousel css file for our team section -->
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+
+
+	<!----Linking google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+	<!----font-awsome start-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!----font-awsome ends-->
+
+		<!----css file link-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	   <!----favicon setting-->
+	<link rel="shortcut icon" type="text/css" href="img/mylogo.png">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!----magnific popup js file for work section -->
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+
+	<!----owlcarousel js file for our team section -->
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!----------email notification-------->
+
+
+
+
+
+
+
+<!-- <link rel="stylesheet" href="assets/tether/tether.min.css">
+ 
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css"> -->
+ 
+
+
+
+
+
+	<style type="text/css">
+	
+	.servicebody
+	{
+		
+
+	}
+
+
+	html
+	{
+		scroll-behavior: smooth;
+	}
+	/*footer code start */
+/* Define styles for the footer */
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 40px 0;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.footer-logo img {
+  max-width: 100px;
+}
+
+.footer-info p {
+  font-size: 16px;
+  margin: 10px 0;
+}
+
+.footer-contact {
+  flex: 1;
+  margin-left: 20px;
+  max-width: 300px;
+}
+
+.footer-contact h4 {
+  font-size: 18px;
+  margin-top: 0;
+}
+
+.footer-contact address {
+  font-size: 14px;
+}
+
+.footer-contact a {
+  color: #fff;
+  text-decoration: none;
+  display: block;
+  margin-bottom: 5px;
+}
+
+.footer-contact a:hover {
+  text-decoration: underline;
+}
+
+.footer-copyright p {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 14px;
+}
+
+	/*footer code end */
+	</style>
+
+</head>
+<body onload="myfunction()">
+		   <!---preloader starts	----->
+
+		   <div id="loading"></div>
+
+		   <!---preloader Ends	----->
+
+
+			<!---Navigation Starts	----->
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<!------Responsive Button---->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navi">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+
+
+				</button>
+
+				<h1 style="color: white;margin-top: 10px;" id="myhead">Learnifyy</h1>
+			</div>
+			<div class="collapse navbar-collapse" id="navi">
+                 <!------Navigation menus starts---->
+				<ul class="nav navbar-nav navbar-right">
+					<li> <a href="">Home</a></li>
+					<li> <a href="#myservice_section">Our Service</a></li>
+					<li> <a href="#myfaq">FAQs</a></li>
+					<!-- <li> <a href="">Carrier</a></li> -->
+					<li> <a href="logout.php" id="our-location" class="btn-success" >Log-out</a></li>
+					<!-- <li> <a href="#" id="our-location" class="btn-success" >Welcome  ></a></li> -->
+				</ul>
+	                 <!------Navigation menus ends---->
+			</div>
+		</div>
+	</nav>
+			<!---Navigation Ends	----->
+
+			<!---Slider Section starts	------->
+			<section class="slider text-center" id="slider">
+				<div class="slider-overlay">
+					<div class="slider-content">
+						<div class="icons">
+						<h1><b>Welcome  <?php echo $_SESSION['username'];   ?><br><br><br></b></h1>
+							<i class="fa fa-apple"></i>
+							<i class="fa fa-android"></i>
+							<i class="fa fa-windows"></i>
+						</div>
+						<br>
+						<div class="text">     
+							 <!-- jquery typed effect -->
+							
+						</div>
+							<div class="cta-div">
+								<!-- <a href="" class="btn1">CONTACT US</a> -->
+								<a href="#myservice_section" class="btn2">LEARN TODAY</a>
+							</div>
+							<br><br>
+							<div class="social-networks">
+								<a href="" class="fa fa-facebook"></a>
+								<a href="" class="fa fa-twitter"></a>
+								<a href="" class="fa fa-instagram"></a>
+								<a href="" class="fa fa-reddit"></a>
+								<a href="" class="fa fa-linkedin"></a>
+								<a href="" class="fa fa-cog"></a>
+							</div>
+					</div>
+				</div>
+			</section>
+
+			<!---Slider Section ends------->
+
+			<!---Login Start------->
+
+			<div class="modal fade modal-dialog-centered" id="mymodal">
+				<div class="modal-dialog ">
+					<div class="modal-content">
+						<h3 id="login-heading">Login</h3>
+						
+					<div class="modal-body" >
+						<div class="left-box">
+						<form method="POST" action="validation.php">
+							<div class="form-group">
+								<label><i class="fa fa-user fa-2x"></i>Username :</label>
+								<input type="text" name="name" class="form-control">
+
+								<label><i class="fa fa-lock fa-2x"></i>Password :</label>
+								<input type="password" name="password" class="form-control">
+								<button id="btn-login" type="submit">Login</button>
+								
+							</div>
+							<div class="register">
+								<h2>Don't have an account?&nbsp<span id="create-account"><a href="signup.html">Create</span></a> </h2>
+							</div>
+							
+						</form>
+					</div>
+					<div class="right-box">
+						<span class="signinwith">Sign in With <br> Social Networks</span>
+
+						<button class="social facebook">Log in with Facebook</button>
+						<button class="social twitter">Log in with twitter</button>
+						<button class="social google">Log in with gmail</button>
+					</div>
+						
+					</div>
+					
+						
+				</div>
+			</div>
+		</div>
+
+         <!---Login Ends------->
+
+         <!---Our Services Section Start------->
+
+         <br><br>
+         <div class="container-fluid servicebody" id="myservice_section">
+         <div class="service-are" id="service">
+         	<div class="row">
+         		<div class="col-xs-12">
+         			<div class="section-title text-center">
+         				<h2><b>SERVICES</b></h2>
+         				<p>
+         					this is our serices theses are the services provided by us <br>this are the services provided by us
+         				</p>
+         			</div>
+         		</div>
+         	</div>
+         	<div class="row">
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-leaf"></i>
+         				</div>
+         				<h3><a href="programmingdemo.php">PROGRAMMING</a></h3>
+         				<p>
+         					Here you will find all the lecture tutorials related to programming languages 
+         					like JAVA,PYTHON,ANDROID etc
+         				</p>
+         			</div>
+         		</div>
+
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="video tutorials/java/display_video_courses.php">VIDEO TUTORIALS</a></h3>
+         				<p>
+         					Here you will find all the videos tutorials related to programming languages 
+         					like JAVA,PYTHON,ANDROID etc
+         				</p>
+         			</div>
+         		</div>
+
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="online_quize/quizhome.php">EXERCISE</a></h3>   <!--  exercise/exercise.php -->
+         				<p>
+         					Here you will find problem programs for practice and their implementation also which will improve your coding skill
+         				</p>
+         			</div>
+         		</div>
+
+  
+
+			<!-- introduction video section ends -->
+<!-- =============================================================================================================================== -->
+			<!---FAQs Section Start------->
+
+			<br><br><br>
+			<section class="faq" id="myfaq">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h2><b>GENERAL FAQs</b></h2>
+							<div class="sub-heading">
+								<p>
+									you can ask the regarding the issues.we will <br>solve that together for sure
+								</p>
+								
+							</div>
+						</div>	
+
+					</div>
+				</div> <br><br><br>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7">
+							<div class="panel-group" id="accordian">
+
+
+								<?php 
+
+								  $con=mysqli_connect('localhost','root');
+									   if (!$con) {
+									   	die('connection failed'.mysqli_connect_error());
+									   }
+
+									mysqli_select_db($con,'uniquedeveloper');
+
+									$sql="select * from faq";
+									$result=mysqli_query($con,$sql);
+									while ($row=mysqli_fetch_array($result))
+									{
+					?>
+
+								<div class="panel panel-default">
+									<div class="panel-heading" id="headingOne">
+										<h4 class="panel-title">
+											<a href="#<?php echo $row['id']; ?>" data-toggle="collapse" class="collapse" data-parent="#accordian"><?php echo $row['faq_title']; ?></a>
+										</h4>
+									</div>
+									<div id="<?php echo $row['id']; ?>" class="panel-collapse collapse " aria-labelledby="headingOne">
+										<div class="panel-body">
+											<p>
+												<?php echo $row['faq_description']; ?>
+											</p>
+										</div>
+									</div>
+								</div>
+
+							<?php } ?>
+
+
+
+								<div class="panel panel-default">
+									<div class="panel-heading" id="headingTwo">
+										<h4 class="panel-title">
+											<a href="#collapseTwo" class="collapsed" data-toggle="collapse" data-parent="#accordian">How does it works ?</a>
+										</h4>
+									</div>
+									<div id="collapseTwo" class="panel-collapse collapse" aria-labelledby="headingTwo">
+										<div class="panel-body">
+											<p>
+													Learnifyy is an online E-learning website <br>
+												here you can learn programming languages,Tech News and improve your coding skill	
+											</p>
+										</div>
+									</div>
+								</div>
+
+
+
+
+								
+							</div>
+						</div>
+
+						<div class="freeimage" id="meimg">
+							<div class="col-md-2 col-md-offset">
+								<img src="img/faq1.png">
+							</div>
+						</div>
+
+					</div>
+				</div>
+				
+
+			</section>
+
+
+			<!---FAQs Section Ends------->
+
+
+			<!---Contact us Section Start------->
+
+
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="row text-center">
+	<!-- <h2><b>CONTACT US</b></h2><br><br>
+	<center>
+	<div class="card" style="width: 30rem;">
+  <div class="card-body border-info">
+   
+   <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">message</label>
+    <input type="password" class="form-control"  placeholder="enter your message">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </div> -->
+</div></center>
+</div>
+
+    
+
+
+
+			<!---Contact us Section Ends------->
+
+
+
+
+ 			<!---footer Section Start	----->
+
+
+		<!-- <footer class="section footer-classic context-dark bg-image" style="background: SandyBrown">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light" src="img/joker.png" alt="" width="140" height="37" srcset="images/agency/logo-retina-inverse-280x74.png 2x"></a> 
+                <p>	Learnifyy is an excellent learning hub, dedicated to the programmer who want to explore.</p>
+               
+                <p class="rights"><span>©  </span><span class="copyright-year">2023</span><span> </span><span>Learnifyy</span><span>. </span><span>All Rights Reserved.</span></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5>Contacts</h5>
+              <dl class="contact-list">
+                <dt>Address:</dt>
+                <dd>Vishwakarma Institute of technology, Pune</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>email:</dt>
+                <dd>
+				<a href="mailto:#">yash.gaikwad22@vit.edu</a>
+				<a href="mailto:#">shravani.phadol22@vit.edu</a>
+				<a href="mailto:#">shubhechha.mehere22@vit.edu</a>
+				<a href="mailto:#">prerna.divekar22@vit.edu</a>
+			</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>phones:</dt>
+                <dd><a href="tel:#">+91 8080261490</a> <span>or</span> <a href="tel:#">+91 9881264564</a>
+                </dd>
+              </dl>
+            </div>
+          
+          </div>
+        </div>
+      
+      </footer> -->
+
+	  <footer class="footer">
+  <div class="footer-content">
+    <div class="footer-info">
+      <h3>Learnifyy</h3>
+      <p>An excellent learning hub, dedicated to programmers who want to explore.</p>
+    </div>
+    
+    <div class="footer-contacts">
+      <h4>Contact Us</h4>
+      <address>
+        Vishwakarma Institute of Technology, Pune<br>
+        <a href="mailto:saloni.khedekar22@vit.edu">saloni.khedekar22@vit.edu</a><br>
+        <a href="mailto:shravani.phadol22@vit.edu">shravani.phadol22@vit.edu</a><br>
+        <a href="mailto:shubhechha.mehere22@vit.edu">shubhechha.mehere22@vit.edu</a><br>
+        <a href="mailto:prerna.divekar22@vit.edu">prerna.divekar22@vit.edu</a>
+      </address>
+      <p>
+        <a href="tel:+918080261490">+91 8080261490</a> or <a href="tel:+919881264564">+91 9881264564</a>
+      </p>
+    </div>
+  </div>
+  
+  <div class="footer-copyright">
+    <p>&copy; 2023 Learnifyy. All Rights Reserved.</p>
+  </div>
+</footer>
+
+
+
+ 			<!---footer Section Ends	----->
+
+
+ 			<!---footer Section Ends	----->
+
+
+
+  			<!---This is script section------->
+
+<script type="text/javascript">
+	
+	var preloader=document.getElementById('loading');
+	function myfunction()
+	 {
+		preloader.style.display='none';
+	}
+
+
+	function addButton() {
+		var body=document.getElementsByTagName('body')[0];
+		var myfaq1=document.getElementById('myfaq');
+		var btn=document.createElement('button');
+		btn.innerHTML='sunil';
+		myfaq1.appendChild(btn);
+		body.appendChild(myfaq);
+	}
+
+
+
+</script>
+
+<script src="js/jquery.ripples-min.js" type="text/javascript"></script>
+<script src="js/typed.min.js" type="text/javascript"></script>
+<script src="js/main.js" type="text/javascript"></script>
+
+
+
+
+
+
+<!--   <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script> -->
+
+</body>
+</html>
